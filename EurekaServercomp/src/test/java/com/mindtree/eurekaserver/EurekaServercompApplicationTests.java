c0fb5ec26e3d3c77a0e6e958dc0ce5db01aa1265
BhargavNarla <113984629+BhargavNarla@users.noreply.github.com>
2022-09-20 16:21:18 +0530

    Add files via upload

---
+++ EurekaServercomp/src/test/java/com/mindtree/eurekaserver/EurekaServercompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServercompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
